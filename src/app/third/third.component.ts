45aa43f7feee44ce75d490004aa400237ec19f3c
Valeriy <goldaboss@yandex.ru>
2020-10-18 23:22:41 +0300

    third *ngFor capabilities

--- src/app/third/third.component.ts
+++ src/app/third/third.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Capabilities {
+  icon: string;
+  text: string;
+}
+
 @Component({
   selector: 'app-third',
   templateUrl: './third.component.html',
@@ -8,6 +13,15 @@ import { Component, OnInit } from '@angular/core';
 export class ThirdComponent implements OnInit {
 
   constructor() { }
+
+  capabilities: Capabilities[] = [
+    {icon: 'icon-Union-1', text: 'Минимизировать временные издержки при выпуске сертификатов'},
+    {icon: 'icon-Union-2', text: 'Обойтись минимальными человеческими ресурсами и при этом зарабатывать больше'},
+    {icon: 'icon-Union-3', text: 'Выстроить партнёрскую сеть по всей РФ'},
+    {icon: 'icon-Union-4', text: 'Автоматизировать все рутинные операции'},
+    {icon: 'icon-Union-5', text: 'Избежать ошибок, связанных с человеческим фактором, благодаря продуманной автоматизации'},
+    {icon: 'icon-Union-6', text: 'Выпускать «облачные» сертификаты электронной подписи'},
+];
 
   ngOnInit(): void {
   }
