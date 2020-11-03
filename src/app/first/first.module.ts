b6df5dcbbb8021d775a72b14f02869b6ff7a2316
Valeriy <goldaboss@yandex.ru>
2020-11-03 23:39:51 +0300

    first main and footer section

--- src/app/first/first.module.ts
+++ src/app/first/first.module.ts
@@ -2,12 +2,16 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FirstComponent} from './components/first.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     FirstComponent,
     HeaderComponent,
+    MainComponent,
+    FooterComponent,
   ],
   imports: [
     CommonModule
