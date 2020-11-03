b3fa51275c1c41db79f2672a2d6896c127f5b1c1
Valeriy <goldaboss@yandex.ru>
2020-11-03 21:40:42 +0300

    refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FirstComponent } from './first/first.component';
-import { SecondComponent } from './second/second.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ThirdComponent } from './third/third.component';
-import { FourthComponent } from './fourth/fourth.component';
-import { FifthComponent } from './fifth/fifth.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FirstComponent} from './first/first.component';
+import {SecondComponent} from './second/second.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {ThirdComponent} from './third/third.component';
+import {FourthComponent} from './fourth/fourth.component';
+import {FifthComponent} from './fifth/fifth.component';
 import {CapabilitiesItemComponent} from './third/capabilities-item/capabilities-item.component';
 import {CapabilitiesListComponent} from './third/capabilities-list/capabilities-list.component';
 import {SixthComponent} from './sixth/sixth.component';
-import { SixthMainComponent } from './sixth/sixth-main/sixth-main.component';
-import { FooterComponent } from './footer/footer.component';
-
+import {SixthMainComponent} from './sixth/sixth-main/sixth-main.component';
+import {FooterComponent} from './footer/footer.component';
 
 
 @NgModule({
@@ -39,4 +38,5 @@ import { FooterComponent } from './footer/footer.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
